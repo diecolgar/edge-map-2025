8a96f0fbf06ffef02b0a7ae2e7fd2f31901e65a2
diecolgar <diecolgar@gmail.com>
2025-04-28 12:16:24 +0200

    Bigger tap area on Map/List Menu

--- app/components/BottomBar.jsx
+++ app/components/BottomBar.jsx
@@ -4,9 +4,9 @@ import React from "react";
 const BottomBar = ({ activeView, onChangeView }) => {
   return (
     <div className="absolute bottom-0 left-0 w-full z-[1000] bg-[#323232] h-14 flex flex-col items-center">
-      {/* Contenedor del indicador: ocupa el 100% del ancho */}
-      <div className="relative w-full h-2 mb-2">
-        {/* Div indicador: ocupa el 50% y se mueve según activeView */}
+      {/* Contenedor del indicador */}
+      <div className="relative w-full h-1">
+        {/* Indicador animado */}
         <motion.div
           className="absolute top-0 h-1 bg-green-400"
           style={{ width: "50%" }}
@@ -15,12 +15,12 @@ const BottomBar = ({ activeView, onChangeView }) => {
         />
       </div>
       {/* Sección de botones */}
-      <div className="flex justify-around items-center w-full">
+      <div className="flex justify-around items-center w-full flex-1">
         {["map", "list"].map((view) => (
           <button
             key={view}
             onClick={() => onChangeView(view)}
-            className="text-lg font-semibold text-white px-4"
+            className="text-lg font-semibold text-white w-1/2 h-full flex items-center justify-center"
           >
             {view === "map" ? "Map" : "List"}
           </button>
